9e650754d8cc43d8cf1d97ee4ee43e62b1252b22
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-05-05 17:41:56 +0700

    feat: add image uploader

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
 import streamlit as st
+
+st.title("AI Image Classifier")
+
+# Store the uploaded file
+uploaded_file = st.file_uploader("Choose a file", type=["png", "jpg", "jpeg"])
+
+# Display the uploaded image
+if uploaded_file is not None:
+    st.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
